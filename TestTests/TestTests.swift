019589258fdca8a919317d3f8d6a215327ca467f
Daniels Klinge <daniels.klinge@kevin.eu>
2023-09-28 15:47:45 +0300

    Test

--- TestTests/TestTests.swift
+++ TestTests/TestTests.swift
@@ -10,7 +10,6 @@ import XCTest
 final class TestTests: XCTestCase {
 
     // This is a dummy unit test to test CI/CD
-    // Really, it tests nothing
     func testExample() throws {
         let test = "test"
         
