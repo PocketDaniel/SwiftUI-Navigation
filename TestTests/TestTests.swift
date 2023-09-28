9a109d938a84d3e9c1c1e67e3adaae75c3e7e018
Daniels Klinge <daniels.klinge@kevin.eu>
2023-09-28 14:57:52 +0300

    Added dummy unit test

---
+++ TestTests/TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTests.swift
+//  TestTests
+//
+//  Created by Daniel Klinge on 28/09/2023.
+//
+
+import XCTest
+
+final class TestTests: XCTestCase {
+
+    func testExample() throws {
+        let test = "test"
+        
+        XCTAssertEqual("test", test)
+    }
+}
